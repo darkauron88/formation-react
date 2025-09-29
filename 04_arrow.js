ddcae893af3ba12509627a02e99a96c52031ea88
dche1052 <damien.cherriere.externe@darva.com>
2025-09-29 11:24:48 +0200

    ternaire

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,7 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (i) => { return i++}
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +18,6 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = i => i++
 
 module.exports = {arrow1, arrow2};
